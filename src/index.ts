2d073297b4d037d7e22066d936dd88c0aab6f083
Lipraty <sis1943@outlook.com>
2023-01-19 17:57:15 +0800

    chore: bump 0.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import { Context, Schema, h } from 'koishi'
-import cron from 'node-cron'
-
-interface ResinManageJson {
-  updateNumber: number
-  updateAt: number
-}
 
 declare module 'koishi' {
   interface User {
-    resin: ResinManageJson
+    resin: ResinDatabase
   }
+}
+
+interface ResinDatabase {
+  updateNumber: number
+  updateAt: number
 }
 
 export const name = 'genshin-resin'
@@ -34,80 +33,60 @@ export const Config: Schema<Config> = Schema.object({
 
 export function apply(ctx: Context, config: Config) {
   const logger = ctx.logger('genshin-resin')
+  
+
+  ctx.i18n.define('zh', require('./i18n/zh'))
 
   ctx.model.extend('user', {
     resin: 'json'
   })
 
-  ctx.command('resin [number]', '记录或获取树脂')
+  ctx.command('resin [number]')
     .alias('树脂', '体力', 'tili', 'tl')
-    .userFields(['id', 'resin'])
+    .userFields(['resin'])
     .action(async ({ session }, _resin) => {
       if (_resin) {
+        const resin = Math.trunc(+_resin)
+        if (resin > 160)
+          return session.text('commands.resin.msg.errorNum')
         const resinInterval: number = 8
-        const resin = +_resin
-        if(resin > 160){
-          return '错误的树脂数量，上限为 160 个'
-        }
         const now = Date.now()
-        const durationMinute = (config.noticeThreshold - resin) * resinInterval
-        const cramDate = formatTime(now, durationMinute).split(':')
-        // 更新记录时间
+        // if (session.user.resin.updateAt && new Date(now).getHours() - new Date(session.user.resin.updateAt).getHours() <= 1) {
+        //   return session.text('commands.resin.msg.errorMany')
+        // }
+        const durationTime = (config.noticeThreshold - resin) * resinInterval * 60 * 1000 //ms
+        const futureTime = new Date(now + durationTime)
+        const priv = session.subtype === 'prevate'
         session.user.resin = {
           updateNumber: resin,
           updateAt: now
         }
-        session.send(`保存成功！你当前树脂为 ${resin} 个，将在 ${cramDate[0]} 小时 ${cramDate[1].length === 1 ? '0' + cramDate[1] : cramDate[1]} 分钟后达到预设的 ${config.noticeThreshold} 个。`)
-        try {
-          if (config.recordNotice) {
-            const durationMax = (160 - resin) * resinInterval
-            cron.schedule(formatCron(durationMax), () => {
-              if (session.subtype === 'prevate')
-                session.send(`树脂已恢复到 ${config.noticeThreshold} 个，请及时清理哦！`)
-              else
-                session.send(`${h('at', { id: session.userId })}，你的树脂已恢复到 ${config.noticeThreshold} 个，请及时清理哦！`)
-            })
-          }
-          cron.schedule(formatCron(durationMinute), () => {
-            if (session.subtype === 'prevate')
-              session.send(`树脂已恢复到 ${config.noticeThreshold} 个，请及时清理哦！`)
-            else
-              session.send(`${h('at', { id: session.userId })}，你的树脂已恢复到 ${config.noticeThreshold} 个，请及时清理哦！`)
-          })
-        } catch (error) {
-          logger.error('fail executing cron:', error)
+        const today = session.text('commands.resin.msg.saved.today'), nextday = session.text('commands.resin.msg.saved.nextday')
+        session.send(
+          session.text(
+            'commands.resin.msg.saved.content',
+            [resin, config.noticeThreshold, futureTime.getDay() - new Date(now).getDay() === 0 ? today : nextday, futureTime.getHours(), futureTime.getMinutes()]
+          ))
+        if (config.recordNotice) {
+          ctx.setTimeout(() => {
+            session.send(session.text('commands.resin.msg.overflow', [config.noticeThreshold, priv ? '' : h('at', { id: session.userId })]))
+          }, durationTime + ((160 - config.noticeThreshold) * resinInterval))
         }
+        ctx.setTimeout(() => {
+          session.send(session.text('commands.resin.msg.filled', [config.noticeThreshold, priv ? '' : h('at', { id: session.userId })]))
+        }, durationTime)
       } else {
-        const created = Math.round(session.user.resin.updateNumber + calcTimeCount(new Date(), new Date(session.user.resin.updateAt)))
-        session.send(`树脂已恢复到 ${created} 个${created >= 120 ? '，记得及时清理哦！' : ''}`)
+        const created = Math.round(session.user.resin.updateNumber + calcIntervalNumber(new Date(), new Date(session.user.resin.updateAt)))
+        if (created >= 160) {
+          session.send(session.text('commands.resin.msg2.overflow', [created - 160]))
+        } else {
+          session.send(session.text('commands.resin.msg2.status.content', [created, created >= 120 ? session.text('commands.resin.msg2.status.filled') : '']))
+        }
       }
     })
 }
 
-/**
- * 将分钟转为 GNU crontab 时间格式
- * 
- * 由于最高仅 160 * 8 ≈ 21 小时，不考虑多一天的情况
- */
-function formatCron(minute: number) {
-  if (minute > 59) {
-    const T = (minute / 60).toString().split('.')
-    return `* ${roundMinute(+T[1])} ${T[0]} * * *`
-  } else {
-    return `* ${minute} * * * *`
-  }
-}
-
-function formatTime(now: number, minute: number) {
-  const interval = ((new Date(now).setMinutes(new Date(now).getMinutes() + minute) - now) / 1000 / 60 / 60)
-  const t = interval.toString().split('.')
-  return [t[0], t[1] ? roundMinute(+t[1]) : 0].join(':')
-}
-
-/** 计算时间内可生成树脂数量 */
-function calcTimeCount(newTime: Date, oldTime: Date) {
+function calcIntervalNumber(newTime: Date, oldTime: Date) {
   const interval = newTime.getTime() - oldTime.getTime()
   return (interval / 1000 / 60) / 8
 }
-
-const roundMinute = (minute: number) => Math.round(60 * (+('0.' + minute)))
